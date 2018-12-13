3af9aa76952bc02f175ee6b1547905fc9b7db061
Jmo5896 <jmo5896@moore-clan.net>
2018-12-12 22:49:34 -0700

    I believe I completed the pyBank task, it's even printing

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,3 +11,45 @@
 # The greatest decrease in losses (date and amount) over the entire period
 
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+
+import csv
+
+csv_path = 'budget_data.csv'
+csv_output = 'results.txt'
+total_months = 0
+net_total = 0
+array_of_diferrence =[]
+average_PL = 0
+greatest_increase = [0,0] #date and amount
+greatest_decrease = [0,0] #date and amount
+prev_row = 867884
+
+with open(csv_path, 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        total_months = total_months +1
+        net_total = net_total + int(row[1])
+        array_of_diferrence.append(int(row[1]) - int(prev_row))
+        if (int(row[1]) - int(prev_row)) > int(greatest_increase[1]):
+            greatest_increase = [row[0], row[1]]
+        if (int(row[1]) - int(prev_row)) < int(greatest_decrease[1]):
+            greatest_decrease = [row[0], row[1]]
+        prev_row = row[1]
+    print(f"Total number of months: {total_months}")
+    print(f"Total net amount of Profit/Losses: {net_total}")
+    print(f"Average change in Profit/Losses: {int(sum(array_of_diferrence) / len(array_of_diferrence))}")
+    print(f"The greatest increase in profits:\n   Date: {greatest_increase[0]}\n   Amount: {greatest_increase[1]}")
+    print(f"The greatest decrease in losses:\n   Date: {greatest_decrease[0]}\n   Amount: {greatest_decrease[1]}")
+
+def put_in_text():
+    return f"""Total number of months: {total_months}\n===========================\n
+Total net amount of Profit/Losses: {net_total}\n===========================\n
+Average change in Profit/Losses: {int(sum(array_of_diferrence) / len(array_of_diferrence))}\n===========================\n
+The greatest increase in profits:\n   Date: {greatest_increase[0]}\n   Amount: {greatest_increase[1]}\n===========================\n
+The greatest decrease in losses:\n   Date: {greatest_decrease[0]}\n   Amount: {greatest_decrease[1]}\n===========================\n"""
+output = put_in_text()
+file = open("results.txt","w")
+file.write(output)
+file.close()
