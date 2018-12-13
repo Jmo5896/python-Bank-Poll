6eafd5c005bace6079b09e7b5a51cde15e2f1d34
Jmo5896 <jmo5896@moore-clan.net>
2018-12-13 11:10:13 -0700

    finished the pypoll assignment and added notes to both/ cleaned up the script a bit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,8 @@
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-
-# The total number of months included in the dataset
-
-# The total net amount of "Profit/Losses" over the entire period
-
-# The average change in "Profit/Losses" between months over the entire period
-
-# The greatest increase in profits (date and amount) over the entire period
-
-# The greatest decrease in losses (date and amount) over the entire period
-
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
-
+# modules imported
 import csv
 
+
+# global variables
 csv_path = 'budget_data.csv'
 total_months = 0
 net_total = 0
@@ -24,9 +12,12 @@ greatest_increase = [0,0] #date and amount
 greatest_decrease = [0,0] #date and amount
 prev_row = 867884
 
+# preparing to read through csv doc
 with open(csv_path, 'r') as f:
     reader = csv.reader(f)
     next(reader)
+
+    # looping through csv file looking for: total months, net total, change from month to month, finding greatest increase and decrease
     for row in reader:
         total_months = total_months +1
         net_total = net_total + int(row[1])
@@ -36,18 +27,22 @@ with open(csv_path, 'r') as f:
         if (int(row[1]) - int(prev_row)) < int(greatest_decrease[1]):
             greatest_decrease = [row[0], row[1]]
         prev_row = row[1]
+
+    # printing all the outputs 
     print(f"Total number of months: {total_months}")
     print(f"Total net amount of Profit/Losses: {net_total}")
     print(f"Average change in Profit/Losses: {int(sum(array_of_diferrence) / len(array_of_diferrence))}")
     print(f"The greatest increase in profits:\n   Date: {greatest_increase[0]}\n   Amount: {greatest_increase[1]}")
     print(f"The greatest decrease in losses:\n   Date: {greatest_decrease[0]}\n   Amount: {greatest_decrease[1]}")
 
+
+# function and function call to print results to a .txt file 
 def put_in_text():
     return f"""Total number of months: {total_months}\n===========================\n
 Total net amount of Profit/Losses: {net_total}\n===========================\n
 Average change in Profit/Losses: {int(sum(array_of_diferrence) / len(array_of_diferrence))}\n===========================\n
 The greatest increase in profits:\n   Date: {greatest_increase[0]}\n   Amount: {greatest_increase[1]}\n===========================\n
-The greatest decrease in losses:\n   Date: {greatest_decrease[0]}\n   Amount: {greatest_decrease[1]}\n===========================\n"""
+The greatest decrease in losses:\n   Date: {greatest_decrease[0]}\n   Amount: {greatest_decrease[1]}"""
 output = put_in_text()
 file = open("results.txt","w")
 file.write(output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,20 @@
-# You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
-
-# The total number of votes cast
-
-# A complete list of candidates who received votes
-
-# The percentage of votes each candidate won
-
-# The total number of votes each candidate won
-
-# The winner of the election based on popular vote.
-
-# In addition, your final script should both print the analysis to the terminal and export a text file with the results.
-
+# modules imported:
 import csv
 
+# global variables
 csv_path = 'election_data.csv'
 total_votes = 0
 set_of_candidates = set()
 list_of_candidates =[]
 total_percentages = {}
+winner = ['', 0]
 
+# setting up to read through csv file
 with open(csv_path, 'r') as f:
     reader = csv.reader(f)
     next(reader)
+
+    # looping through csv file for: total votes, Candidates voted for, and tallying votes
     for row in reader:
         total_votes = total_votes + 1
         set_of_candidates.add(str(row[2]))
@@ -30,15 +22,34 @@ with open(csv_path, 'r') as f:
             total_percentages[row[2]] += 1
         else:
             total_percentages[row[2]] = 1
+
+    # converting my set of candidates to a list to use more easily in the printing and determining a winner 
     list_of_candidates = list(set_of_candidates)
+
+    # small loop for figurig out who had the most votes
+    for candidate in list_of_candidates:
+        if total_percentages[candidate] > winner[1]:
+            winner[0] = candidate
+            winner[1] = total_percentages[candidate]
+
+    # printing all the outputs 
     print(f"Total number of votes: {total_votes}") 
     print(f"Total number of candidates: {', '.join(list_of_candidates)}")
-    print(f"Total percentage of votes for {list_of_candidates[0]}: {(total_percentages[list_of_candidates[0]]/total_votes) * 100}%")
-    print(f"Total percentage of votes for {list_of_candidates[1]}: {(total_percentages[list_of_candidates[1]]/total_votes) * 100}%")
-    print(f"Total percentage of votes for {list_of_candidates[2]}: {(total_percentages[list_of_candidates[2]]/total_votes) * 100}%")
-    print(f"Total percentage of votes for {list_of_candidates[3]}: {(total_percentages.get(list_of_candidates[3])/total_votes) * 100}%")
+    print(f"Total percentage/count of votes for {list_of_candidates[0]}: {(total_percentages[list_of_candidates[0]]/total_votes) * 100}%, {total_percentages[list_of_candidates[0]]}")
+    print(f"Total percentage/count of votes for {list_of_candidates[1]}: {(total_percentages[list_of_candidates[1]]/total_votes) * 100}%, {total_percentages[list_of_candidates[1]]}")
+    print(f"Total percentage/count of votes for {list_of_candidates[2]}: {(total_percentages[list_of_candidates[2]]/total_votes) * 100}%, {total_percentages[list_of_candidates[2]]}")
+    print(f"Total percentage/count of votes for {list_of_candidates[3]}: {(total_percentages[list_of_candidates[3]]/total_votes) * 100}%, {total_percentages[list_of_candidates[3]]}")
+    print(f"The winner of the election: {winner[0]}")
+
+# function and function call to print results to a .txt file 
 def put_in_text():
-    return f"""hello world"""
+    return f"""Total number of votes: {total_votes}\n===========================\n
+Total number of candidates: {', '.join(list_of_candidates)}\n===========================\n
+Total percentage/count of votes for {list_of_candidates[0]}: {(total_percentages[list_of_candidates[0]]/total_votes) * 100}%, {total_percentages[list_of_candidates[0]]}\n===========================\n
+Total percentage/count of votes for {list_of_candidates[1]}: {(total_percentages[list_of_candidates[1]]/total_votes) * 100}%, {total_percentages[list_of_candidates[1]]}\n===========================\n
+Total percentage/count of votes for {list_of_candidates[2]}: {(total_percentages[list_of_candidates[2]]/total_votes) * 100}%, {total_percentages[list_of_candidates[2]]}\n===========================\n
+Total percentage/count of votes for {list_of_candidates[3]}: {(total_percentages[list_of_candidates[3]]/total_votes) * 100}%, {total_percentages[list_of_candidates[3]]}\n===========================\n
+The winner of the election: {winner[0]}"""
 
 output = put_in_text()
 file = open("results.txt","w")
